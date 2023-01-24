2948a173e317b63813a8c10281f7b05a02e63bc0
sumair rana <sumairrana@Hamzas-MacBook-Pro.local>
2023-01-24 09:22:52 -0500

    home

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -5,7 +5,7 @@ export default function FirstPost() {
     <>
       <h1>First Post</h1>
       <h2>
-        <Link href="/">Back to Home</Link>
+        <Link href="/">Back to home</Link>
       </h2>
     </>
   );
